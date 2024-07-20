251461cfe8bc4d1f20e6be438e84535bb4ddef88
Cody Washington <115746975+Jelatinone@users.noreply.github.com>
2024-07-20 04:20:52 -0500

    Apply Spotless

--- src/main/java/org/frc5411/robot2024/Manager.java
+++ src/main/java/org/frc5411/robot2024/Manager.java
@@ -51,7 +51,6 @@ import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.Serial;
 import java.util.List;
-import java.util.Comparator;
 import java.util.Map.Entry;
 import java.util.Objects;
 import java.util.Optional;
